25bb34d1883945d01328cdb3784be11372595e70
Jeonghoon <hxxjxxxhxxx@gmail.com>
2023-01-04 22:06:40 +0900

    ejs, morgan 설치

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan");
 const port = 4000;
 
 const router = require("./routes");
 
+app.use(logger("dev"));
 app.use(express.json());
-app.use("/api", router);
+app.use(express.urlencoded({ extended: false }));
+
+// view 경로설정
+app.set("views", __dirname + "/views");
+
+// 화면 engine을 ejs로 설정
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
 app.use("/", express.static("assets"));
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.status(200).render("index.html");
